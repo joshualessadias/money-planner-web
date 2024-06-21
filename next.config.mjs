c8bbc508dcf57d1c58973da055241899362b1c78
joshua <joshualessadias@gmail.com>
2024-06-21 00:10:33 -0300

    fixing cors error?

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,14 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+module.exports = {
+    async rewrites() {
+        return [
+            {
+                source: '/:path*',
+                destination: process.env.NEXT_PUBLIC_MONEY_PLANNER_API_URL + ':path*',
+            },
+        ]
+    },
+};
