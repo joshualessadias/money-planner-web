80f236d76f6f1b322d53df891f4d3f1ed1829d1e
joshua <joshualessadias@gmail.com>
2024-06-21 00:21:32 -0300

    fixing cors error?

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     async rewrites() {
+        const apiUrl = process.env.NEXT_PUBLIC_MONEY_PLANNER_API_URL;
+        if (typeof apiUrl !== 'string') {
+            throw new Error('NEXT_PUBLIC_MONEY_PLANNER_API_URL is not defined or not a string');
+        }
         return [
             {
                 source: '/:path*',
-                destination: process.env.NEXT_PUBLIC_MONEY_PLANNER_API_URL + ':path*',
+                destination: apiUrl + ':path*',
             },
         ]
     },
